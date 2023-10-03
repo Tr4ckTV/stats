31f5099e93a3f4620843587b61febbe9bce12d89
etudiants <abcd@mail.com>
2023-10-03 16:22:05 +0200

    Init

---
+++ src/Functions/average.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Stats\Functions;
+
+class Average
+{
+    public static function calculate(array $numbers): float
+    {
+        $sum = array_sum($numbers);
+        $count = count($numbers);
+
+        return $sum / $count;
+    }
+}
---
+++ src/Stats.php
@@ -0,0 +1,8 @@
+<?php
+
+use Stats\Functions\Average;
+
+$list = [10, 15, 14, 16, 20];
+
+expect(Average::calculate($list))->toBe(15.0);
+?>
